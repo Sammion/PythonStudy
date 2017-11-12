0b677058017b8ecf06c8dd806fd5b3f4fe495146
Sammion <samqian163@163.com>
2017-11-12 15:37:53 +0800

    学习使用xml.dom创建XML文件

---
+++ source/test33.py
@@ -0,0 +1,14 @@
+'''
+Created on 2017-11-11
+@author： Sam
+@module： test
+@descripution:
+    学习使用xml.dom解析XML文件
+'''
+from xml.dom.minidom import parse
+# import xml.dom.minidom as Dom
+# doc = Dom.Document()
+# class bookscanner:
+#     def __init__(self,doc):
+#         for child in doc.childNodes:
+#             if child.nodeType ==
