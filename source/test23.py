dd979a3b369dffb307313d64511b102500263d56
Sammion <samqian163@163.com>
2018-01-04 10:25:13 +0800

    学习Json解析与封装

--- source/test23.py
+++ source/test23.py
@@ -1,29 +1,32 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Sep 12 10:38:38 2017
-
 @author: shuai.qian
 """
 import json
-d = dict(name = 'Sam', age = 20, score = 88)
+
+d = dict(name='Sam', age=20, score=88)
 print(json.dumps(d))
 
 json_str = '{"age":20, "score":99, "name":"Sam"}'
 print(json.loads(json_str))
+
 
 class Student(object):
     def __init__(self, name, age, score):
         self.name = name
         self.age = age
         self.score = score
-        
+
+
 s = Student('Bob', 20, 100)
-#print (json.dumps(s))
-print (json.dumps(s, default = lambda obj:obj.__dict__))
+# print (json.dumps(s))
+print(json.dumps(s, default=lambda obj: obj.__dict__))
+
 
 def dict2Student(d):
     return Student(d['name'], d['age'], d['score'])
 
-json_str = '{"age": 20, "score": 88, "name": "Bob"}'
-print (json.loads(json_str, object_hook = dict2Student))
 
+json_str = '{"age": 20, "score": 88, "name": "Bob"}'
+print(json.loads(json_str, object_hook=dict2Student))
