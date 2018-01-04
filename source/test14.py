c7d03a146116e5ebe3aac1a7c264afbdd28a6eea
Sammion <samqian163@163.com>
2018-01-04 10:08:48 +0800

    学习Python中类的继承

--- source/test14.py
+++ source/test14.py
@@ -4,45 +4,50 @@ Created on Tue Sep  5 23:00:23 2017
 
 @author: shuai.qian
 """
+
+
 class Animal(object):
     def __init__(self, n):
         self.__name = n
-        
+
     def run(self):
-        print ("%s running!" % self.__name)
+        print("%s running!" % self.__name)
+
 
 class Runnable(object):
     def __init__(self, n):
         self.__name = n
+
     def bark(self):
-        print ('%s barking!' % self.__name)
+        print('%s barking!' % self.__name)
+
 
 class Flyable(object):
-    
     def __init__(self, n):
         self.__name = n
+
     def fly(self):
-        print ('%s flying!' % self.__name)
+        print('%s flying!' % self.__name)
+
 
 class Bird(Flyable, Animal):
     pass
 
+
 class Dog(Runnable, Animal):
     pass
- 
+
 
 d = Dog('dd')
 b = Bird('bb')
 d.bark()
 b.fly()
-#d.run()
-#b.run()
-print ("====================我是分割线========================================")
-print (dir(b))
+# d.run()
+# b.run()
+print("====================我是分割线========================================")
+print(dir(b))
 
-print ("====================我是分割线========================================")
-print (type(b))
-print ("====================我是分割线========================================")
-print (b.__sizeof__())
-
-
+print("====================我是分割线========================================")
+print(type(b))
+print("====================我是分割线========================================")
+print(b.__sizeof__())
