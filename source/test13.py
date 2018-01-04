25a1f5542334098425adf806288a91e1947a0724
Sammion <samqian163@163.com>
2018-01-04 10:07:35 +0800

    学习使用装饰器的面向对象编程

--- source/test13.py
+++ source/test13.py
@@ -5,29 +5,39 @@ Created on Tue Sep  5 22:43:37 2017
 @author: shuai.qian
 """
 
-class student (object):
+
+class student(object):
     pass
 
+
 s = student()
+
+
 def set_age(self, age):
     self.age = age
 
-from types import MethodType 
-s.set_a = MethodType(set_age,s)
+
+from types import MethodType
+
+s.set_a = MethodType(set_age, s)
 s.set_a(19)
-print (s.age)
+print(s.age)
+
 
 def set_score(self, s):
-    self.score  = s
+    self.score = s
+
 
 student.set_score = set_score
 s.set_score(100)
-print (s.score)
+print(s.score)
 
-class person (object):
+
+class person(object):
     @property
     def gg(self):
         return self.__name
+
     @gg.setter
     def gg(self, name):
         if isinstance(name, str):
@@ -35,12 +45,8 @@ class person (object):
             self.__name = name
         else:
             print('Please input correct name!')
-            
+
+
 p = person()
 p.gg = 'sam'
-print (p.gg)
-
-
-
-
-
+print(p.gg)
