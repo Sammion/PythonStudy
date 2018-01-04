bf516a6b56d9b0f4aea05fd08912080bfaf6a3ad
Sammion <samqian163@163.com>
2018-01-04 10:32:54 +0800

    练习生产者和消费者游戏

--- source/test30.py
+++ source/test30.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Sep 14 08:02:32 2017
-
 @author: shuai.qian
 """
+
 
 def consumer():
     r = ''
@@ -14,6 +14,7 @@ def consumer():
         print('[CONSUMER] Consuming %s...' % n)
         r = '200 OK'
 
+
 def produce(c):
     c.send(None)
     n = 0
@@ -23,6 +24,7 @@ def produce(c):
         r = c.send(n)
         print('[PRODUCER] Consumer return: %s' % r)
     c.close()
+
 
 c = consumer()
 produce(c)
