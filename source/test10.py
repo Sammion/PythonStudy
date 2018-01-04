ed6543f8c6961ab6fbf342401a02625221ed0f1d
Sammion <samqian163@163.com>
2018-01-04 10:00:58 +0800

    练习sorted函数

--- source/test10.py
+++ source/test10.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Sep  3 23:07:20 2017
-
 @author: shuai.qian
 """
 
-l = [12,-2,14,2,5,8,-1,-24]
+l = [12, -2, 14, 2, 5, 8, -1, -24]
 s = 'dsajoDjdoaCXABab'
 
-print (sorted(s, key = str.upper, reverse = False))
+print(sorted(s, key=str.upper, reverse=False))
