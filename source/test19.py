cc4f5bd0bd640b2f3f7f9dad7590765371647811
Sammion <samqian163@163.com>
2018-01-04 10:17:30 +0800

    学习编写ORM框架

--- source/test19.py
+++ source/test19.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Sep 11 22:45:14 2017
-
 @author: shuai.qian
 """
+
 
 class Field(object):
     def __init__(self, name, column_type):
         self.name = name
         self.column_type = column_type
+
     def __str__(self):
         return '<%s:%s>' % (self.__class__.__name__, self.name)
 
@@ -21,12 +22,13 @@ class StringField(Field):
 class IntegerField(Field):
     def __init__(self, name):
         super(IntegerField, self).__init__(name, 'bigint')
-        
+
+
 class ModelMetaclass(type):
-    #cls：当前类准备创建的对象
-    #name: 类的名字
-    #bases: 类继承的父类集合
-    #attrs: 类的方法集合
+    # cls：当前类准备创建的对象
+    # name: 类的名字
+    # bases: 类继承的父类集合
+    # attrs: 类的方法集合
     def __new__(cls, name, bases, attrs):
         if name == 'Model':
             return type.__new__(cls, name, bases, attrs)
@@ -34,37 +36,41 @@ class ModelMetaclass(type):
         mappings = dict()
         for k, v in attrs.items():
             if isinstance(v, Field):
-                print('Found mapping: %s ==> %s'%(k,v))
+                print('Found mapping: %s ==> %s' % (k, v))
                 mappings[k] = v
         for k in mappings.keys():
             attrs.pop(k)
         attrs['__mappings__'] = mappings
         attrs['__table__'] = name
         return type.__new__(cls, name, bases, attrs)
-    
-class Model(dict, metaclass = ModelMetaclass):
+
+
+class Model(dict, metaclass=ModelMetaclass):
     def __init__(self, **kw):
-        super(Model,self).__init__(**kw)
+        super(Model, self).__init__(**kw)
+
     def __getattr__(self, key):
         try:
             return self[key]
         except KeyError:
             raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
     def __setattr__(self, key, value):
         self[key] = value
+
     def save(self):
         fields = []
         params = []
         args = []
-        for k,v in self.__mappings__.items():
+        for k, v in self.__mappings__.items():
             fields.append(v.name)
             params.append('?')
             args.append(getattr(self, k, None))
-        sql = 'insert into %s (%s) values (%s)'%(self.__table__,','.join(fields), ','.join(params))
-        print ('SQL: %s' % sql)
-        print ('ARGS: %s' % str(args))
-        
-        
+        sql = 'insert into %s (%s) values (%s)' % (self.__table__, ','.join(fields), ','.join(params))
+        print('SQL: %s' % sql)
+        print('ARGS: %s' % str(args))
+
+
 class User(Model):
     # 定义类的属性到列的映射：
     id = IntegerField('id')
@@ -72,20 +78,8 @@ class User(Model):
     email = StringField('email')
     password = StringField('password')
 
+
 # 创建一个实例：
 u = User(id=12345, name='Model', email='test@orm.org', password='my-pwd')
 # 保存到数据库：
-u.save()       
-        
-        
-        
-        
-        
-        
-        
-
-
-
-
-
-            
+u.save()
