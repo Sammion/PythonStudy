996e4440204748df83bc8c0b1c1c74ccc29819a8
Sammion <samqian163@163.com>
2018-01-04 10:27:19 +0800

    test25 学习多线程编程3

--- source/test26.py
+++ source/test26.py
@@ -7,7 +7,9 @@ Created on Tue Sep 12 21:04:15 2017
 
 import time, threading
 
-balance = 0 
+balance = 0
+
+
 def change(n):
     global balance
     balance = balance + n
@@ -17,13 +19,13 @@ def change(n):
 def run_thread(n):
     for i in range(1000000):
         change(n)
-    
-t1 = threading.Thread(target = run_thread, args = (5,))
-t2 = threading.Thread(target = run_thread, args = (8,))
-  
+
+
+t1 = threading.Thread(target=run_thread, args=(5,))
+t2 = threading.Thread(target=run_thread, args=(8,))
+
 t1.start()
 t2.start()
 t1.join()
 t2.join()
-print (balance)
-    
+print(balance)
