a1b0c31be3cd08efe1080f09c65c7336a4b729f8
Sammion <samqian163@163.com>
2018-01-04 10:28:07 +0800

    学习多线程编程4

--- source/test27.py
+++ source/test27.py
@@ -9,11 +9,13 @@ import threading
 balance = 0
 lock = threading.Lock()
 
+
 def change(n):
     global balance
-    
+
     balance = balance + n
     balance = balance - n
+
 
 def run_thread(n):
     for i in range(1000000):
@@ -23,15 +25,13 @@ def run_thread(n):
         finally:
             lock.release()
 
-t1 = threading.Thread(target = run_thread, args = (5,))
-t2 = threading.Thread(target = run_thread, args = (8,))
-  
+
+t1 = threading.Thread(target=run_thread, args=(5,))
+t2 = threading.Thread(target=run_thread, args=(8,))
+
 t1.start()
 t2.start()
 t1.join()
 t2.join()
 
 print(balance)
-            
-    
-            
