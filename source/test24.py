3e1ffd63c13a16cd942201bf8afe8661ec39c40d
Sammion <samqian163@163.com>
2018-01-04 10:26:12 +0800

    学习多线程编程

--- source/test24.py
+++ source/test24.py
@@ -1,37 +1,38 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Sep 12 11:03:25 2017
-
 @author: shuai.qian
 """
 
 from multiprocessing import Process, Pool
 import os, time, random
-#子进程要执行的代码
+
+
+# 子进程要执行的代码
 def run_proc(name):
-    print ("Run child process %s (%s)"% (name, os.getpid()))
+    print("Run child process %s (%s)" % (name, os.getpid()))
+
 
 def long_time_task(name):
-    print ('Run task %s (%s)......' % (name, os.getpid()))
+    print('Run task %s (%s)......' % (name, os.getpid()))
     start = time.time()
-    time.sleep(random.random()*10)
+    time.sleep(random.random() * 10)
     end = time.time()
-    print ('Task %s runs %0.2f seconds!' % (name, (end-start)))
-    
-    
+    print('Task %s runs %0.2f seconds!' % (name, (end - start)))
+
+
 if __name__ == "__main__":
-    print ("Prarent process %s." % os.getpid())
+    print("Prarent process %s." % os.getpid())
     pl = Pool(50)
     for i in range(10):
-        pl.apply_async(long_time_task, args = (i,))
+        pl.apply_async(long_time_task, args=(i,))
     print('Waiting for all subprocesses done...')
     pl.close()
     pl.join()
-    print ('All subprocesses done.')
-#    p = Process(target = run_proc, args = ('test', ))
-    
+    print('All subprocesses done.')
+# p = Process(target = run_proc, args = ('test', ))
+
 #    print ('Child process will start!')
 #    p.start()
 #    p.join()
 #    print('Child process end!')
-        
