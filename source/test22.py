473a91fa175a2c299fd1a31f7496a6df16448912
Sammion <samqian163@163.com>
2018-01-04 10:24:09 +0800

    学习文件读写

--- source/test22.py
+++ source/test22.py
@@ -1,25 +1,21 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Sep 12 09:41:49 2017
-
 @author: shuai.qian
 """
 import os
 import pickle
+
 f = open('E:\Company\Test\dump.txt', 'wb')
-d = dict(name = 'Sam', age = 22, score = 99)
+d = dict(name='Sam', age=22, score=99)
 pickle.dump(d, f)
 f.close()
 
 data = None
 f = open('E:\Company\Test\3-2\Paqfiles\Paqfile0\ProcessFiles\ProcessFile0\ClassData', 'rb')
-print ('执行我了吗！')
+print('执行我了吗！')
 data = pickle.load(f)
-print ('有没有执行我了呢！')
+print('有没有执行我了呢！')
 f.close()
-    
-print ('反序列化后', data)
 
-
-
-
+print('反序列化后', data)
