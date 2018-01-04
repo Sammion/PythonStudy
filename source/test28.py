76a64b3044fa0a19141887fcc267cb1049034231
Sammion <samqian163@163.com>
2018-01-04 10:29:01 +0800

    学习多线程编程5

--- source/test28.py
+++ source/test28.py
@@ -6,27 +6,28 @@ Created on Wed Sep 13 10:38:35 2017
 """
 
 import threading
+
 local_school = threading.local()
+
 
 def process_student():
     #
     st = local_school.student
-    print ('Hello, %s (in %s)' % (st, threading.current_thread().name))
-    
+    print('Hello, %s (in %s)' % (st, threading.current_thread().name))
+
+
 def process_thread(name):
     #
     local_school.student = name
     process_student()
 
-t1 = threading.Thread(target = process_thread, args = ('Alice', ), name = 'Threa-A')
-t2 = threading.Thread(target = process_thread, args = ('Bob', ), name = 'Threa-B')
+
+t1 = threading.Thread(target=process_thread, args=('Alice',), name='Threa-A')
+t2 = threading.Thread(target=process_thread, args=('Bob',), name='Threa-B')
 
 t1.start()
 t2.start()
 
 t1.join()
 t2.join()
-
-
-    
-    
+   
