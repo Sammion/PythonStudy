de1fbdedd86c1e3fd76b136bff56e954ff779068
Sammion <samqian163@163.com>
2018-01-04 10:16:27 +0800

    练习枚举类

--- source/test18.py
+++ source/test18.py
@@ -7,6 +7,7 @@ Created on Mon Sep 11 22:01:02 2017
 
 from enum import Enum, unique
 
+
 @unique
 class Weekday(Enum):
     sun = 1
@@ -16,28 +17,28 @@ class Weekday(Enum):
     Thu = 5
     Fri = 6
     Sat = 7
-    
-    
+
+
 day1 = Weekday.Mon
-print (day1)
-print (Weekday['Tue'])
-print (day1 == Weekday.Mon)
-print (Weekday(1))
-print (day1 == Weekday(1))
-print (type(day1))
+print(day1)
+print(Weekday['Tue'])
+print(day1 == Weekday.Mon)
+print(Weekday(1))
+print(day1 == Weekday(1))
+print(type(day1))
 for name, member in Weekday.__members__.items():
-    print (name, '=>', member)
+    print(name, '=>', member)
 
 attr1 = 12
 attr2 = 13
-def fn(self, n ):
-    print ('Hello, %s' % n)
-Hello = type('Hello', (object, ), dict(hello = fn,a = attr1, b = attr2))
+
+
+def fn(self, n):
+    print('Hello, %s' % n)
+
+
+Hello = type('Hello', (object,), dict(hello=fn, a=attr1, b=attr2))
 h = Hello()
 h.hello('world！')
-print (h.a)
-print (h.b)
-
-
-
-
+print(h.a)
+print(h.b)
