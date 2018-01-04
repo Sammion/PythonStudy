5f7aa85ae4196015675a14792db073007da15a7c
Sammion <samqian163@163.com>
2018-01-04 10:02:46 +0800

    学习装饰器

--- source/test11.py
+++ source/test11.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Sep  3 23:19:13 2017
-
 @author: shuai.qian
 """
 
+
 def log(fun):
     def wrapper(*args, **kw):
-        print('call %s():' %fun.__name__)
+        print('call %s():' % fun.__name__)
         return fun(*args, **kw)
-    
+
     return wrapper
+
 
 @log
 def now():
     print('Hello world!')
+
 
 now()
