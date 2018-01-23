3c4003454dae1e1009e75dc2e8dd96e00be94e50
Sammion <samqian163@163.com>
2018-01-23 21:50:21 +0800

    常见内建模块学习（六）

---
+++ source/test58.py
@@ -0,0 +1,50 @@
+# @author Sam
+# @date 2018-01-23
+# desc 常见内建模块学习（六）
+# urllib提供了一系列用于操作URL的功能。
+
+from urllib import request
+
+with request.urlopen('https://img19.niuza.com/2018/01/23/20180123195919_44447.jpg-150.jpg') as f:
+    data = f.read()
+    print('status: ', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s: %s' % (k, v))
+        # print('Data: ', data.decode('utf-8'))
+
+# 伪装自己为浏览器，模拟iPhone 6去请求豆瓣首页
+req = request.Request('http://www.douban.com/')
+req.add_header('User-Agent',
+               'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
+with request.urlopen(req) as f:
+    print('Status:', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s: %s' % (k, v))
+    print('Data:', f.read().decode('utf-8'))
+
+
+# 使用post方法登陆豆瓣网
+
+print('Login to weibo.cn...')
+email = input('Email: ')
+passwd = input('Password: ')
+login_data = parse.urlencode([
+    ('username', email),
+    ('password', passwd),
+    ('entry', 'mweibo'),
+    ('client_id', ''),
+    ('savestate', '1'),
+    ('ec', ''),
+    ('pagerefer', 'https://passport.weibo.cn/signin/welcome?entry=mweibo&r=http%3A%2F%2Fm.weibo.cn%2F')
+])
+
+req = request.Request('https://passport.weibo.cn/sso/login')
+req.add_header('Origin', 'https://passport.weibo.cn')
+req.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
+req.add_header('Referer', 'https://passport.weibo.cn/signin/login?entry=mweibo&res=wel&wm=3349&r=http%3A%2F%2Fm.weibo.cn%2F')
+
+with request.urlopen(req, data=login_data.encode('utf-8')) as f:
+    print('Status:', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s: %s' % (k, v))
+    print('Data:', f.read().decode('utf-8'))
