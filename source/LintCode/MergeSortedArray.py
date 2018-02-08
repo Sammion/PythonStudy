f697a51527544b1dcc1fa5b728d4f43ee5e23db5
Sammion <samqian163@163.com>
2018-02-08 11:32:07 +0800

    合并两个排序数组到第一个数组

---
+++ source/LintCode/MergeSortedArray.py
@@ -0,0 +1,40 @@
+class Solution:
+    """
+    @param: A: sorted integer array A which has m elements, but size of A is m+n
+    @param: m: An integer
+    @param: B: sorted integer array B which has n elements
+    @param: n: An integer
+    @return: nothing
+    """
+
+    def mergeSortedArray(self, A, m, B, n):
+        # write your code here
+        m = m - 1
+        n = n - 1
+        l = m + n + 1
+        while m >= 0 or n >= 0:
+            if m < 0:
+                A[l] = B[n]
+                l -= 1
+                n -= 1
+                continue
+            if n < 0:
+                A[l] = A[m]
+                l -= 1
+                m -= 1
+                continue
+            if A[m] > B[n]:
+                A[l] = A[m]
+                l -= 1
+                m -= 1
+            else:
+                A[l] = B[n]
+                l -= 1
+                n -= 1
+        return A
+
+if __name__ == '__main__':
+    s = Solution()
+    A = [1,2,3,0,0]
+    B = [4,5]
+    print(s.mergeSortedArray(A,3,B,2))
